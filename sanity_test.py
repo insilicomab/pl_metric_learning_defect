81a011a87496479cdcb8bd3c8aca411a058010c6
insilicomab <nakada.mitsutaka.c76@kyoto-u.jp>
2023-02-06 19:29:09 +0900

    Update sanity_test.py

--- sanity_test.py
+++ sanity_test.py
@@ -2,9 +2,9 @@ import pytorch_lightning as pl
 import hydra
 from omegaconf import DictConfig
 
-from .src.utils import data_split
-from .src.dataset import ImageDataset
-from .src.model import Net, get_callback
+from src.utils import data_split
+from src.dataset import DataModule
+from src.model import Net, get_callbacks
 
 
 @hydra.main(version_base=None, config_path='config', config_name='config')
@@ -16,7 +16,7 @@ def main(cfg: DictConfig) -> None:
     pl.seed_everything(seed=cfg.seed, workers=True)
 
     # datamodule
-    datamodule = ImageDataset(
+    datamodule = DataModule(
         x_train=x_train,
         y_train=y_train,
         x_val=x_val,
@@ -33,7 +33,7 @@ def main(cfg: DictConfig) -> None:
     trainer = pl.Trainer(
         logger=False,
         max_epochs=cfg.trainer.max_epochs,
-        callbacks=get_callback(config=cfg),
+        callbacks=get_callbacks(config=cfg),
         gpus=cfg.trainer.gpus,
         accumulate_grad_batches=cfg.trainer.accumulate_grad_batches,
         auto_lr_find=cfg.trainer.auto_lr_find,
