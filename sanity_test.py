d6a876c56f96dc924754282c861d94acc8218f44
insilicomab <nakada.mitsutaka.c76@kyoto-u.jp>
2023-02-06 16:30:40 +0900

    Update sanity_test.py

--- sanity_test.py
+++ sanity_test.py
@@ -3,14 +3,31 @@ import hydra
 from omegaconf import DictConfig
 
 from .src.utils import data_split
+from .src.dataset import ImageDataset
+from .src.model import Net
 
 
 @hydra.main(version_base=None, config_path='config', config_name='config')
 def main(cfg: DictConfig) -> None:
     # data split
-    x_train, x_val, y_train, y_val = data_split(config=cfg)
+    x_train, x_val, x_test, y_train, y_val, y_test = data_split(config=cfg)
 
     # set random seed
     pl.seed_everything(seed=cfg.seed, workers=True)
 
+    # datamodule
+    datamodule = ImageDataset(
+        x_train=x_train,
+        y_train=y_train,
+        x_val=x_val,
+        y_val=y_val,
+        x_test=x_test,
+        y_test=y_test,
+        config=cfg
+    )
+
+    # net
+    net = Net(config=cfg)
+
+    
     
