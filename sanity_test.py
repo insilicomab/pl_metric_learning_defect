3aed57428838357a00beec6e66b4ca5aa7cf74cb
insilicomab <nakada.mitsutaka.c76@kyoto-u.jp>
2023-02-06 16:39:57 +0900

    Update sanity_test.py

--- sanity_test.py
+++ sanity_test.py
@@ -4,7 +4,7 @@ from omegaconf import DictConfig
 
 from .src.utils import data_split
 from .src.dataset import ImageDataset
-from .src.model import Net
+from .src.model import Net, get_callback
 
 
 @hydra.main(version_base=None, config_path='config', config_name='config')
@@ -29,5 +29,24 @@ def main(cfg: DictConfig) -> None:
     # net
     net = Net(config=cfg)
 
+    # trainer
+    trainer = pl.Trainer(
+        logger=False,
+        max_epochs=cfg.trainer.max_epochs,
+        callbacks=get_callback(config=cfg),
+        gpus=cfg.trainer.gpus,
+        accumulate_grad_batches=cfg.trainer.accumulate_grad_batches,
+        auto_lr_find=cfg.trainer.auto_lr_find,
+        deterministic=True,
+    )
     
+    # train
+    trainer.fit(net, datamodule=datamodule)
+
+    # test
+    trainer.test(net, datamodule=datamodule, ckpt_path="best")
+
+
+if __name__ == "__main__":
+    main()
     
