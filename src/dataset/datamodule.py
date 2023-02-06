0f49b16d4636b0d395acb04ff23a165e1003ee3a
insilicomab <nakada.mitsutaka.c76@kyoto-u.jp>
2023-02-06 15:15:05 +0900

    add datamodule

---
+++ src/dataset/datamodule.py
@@ -0,0 +1,78 @@
+import torch
+from torch.utils.data import DataLoader
+import pytorch_lightning as pl
+from omegaconf import DictConfig
+
+from .dataset import ImageDataset
+from .transformation import Transforms
+
+
+class DataModule(pl.LightningDataModule):
+    def __init__(
+            self, 
+            x_train: list, 
+            y_train: list, 
+            x_val: list, 
+            y_val: list, 
+            x_test: list,
+            y_test: list,
+            config: DictConfig
+        ):
+        super().__init__()
+        self.x_train = x_train
+        self.y_train = y_train
+        self.x_val = x_val
+        self.y_val = y_val
+        self.x_test = x_test
+        self.y_test = y_test
+        self.config = config
+
+    def setup(self, stage=None):
+        self.train_dataset = ImageDataset(
+            image_name_list=self.x_train,
+            label_list=self.y_train,
+            img_dir=self.config.img_dir,
+            transform=Transforms(config=self.config),
+            phase='train'
+        )
+        self.val_dataset = ImageDataset(
+            image_name_list=self.x_val,
+            label_list=self.y_val,
+            img_dir=self.config.img_dir,
+            transform=Transforms(config=self.config),
+            phase='val'
+        )
+        self.test_dataset = ImageDataset(
+            image_name_list=self.x_test,
+            label_list=self.y_test,
+            img_dir=self.config.img_dir,
+            transform=Transforms(config=self.config),
+            phase='test'
+        )
+
+    def train_dataloader(self):   
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.config.train_dataloader.batch_size,
+            shuffle=self.config.train_dataloader.shuffle,
+            num_workers=self.config.train_dataloader.num_workers,
+            pin_memory=self.config.train_dataloader.pin_memory,
+        )
+    
+    def val_dataloader(self):
+        return DataLoader(
+            self.val_dataset,
+            batch_size=self.config.val_dataloader.batch_size,
+            shuffle=self.config.val_dataloader.shuffle,
+            num_workers=self.config.val_dataloader.num_workers,
+            pin_memory=self.config.val_dataloader.pin_memory,
+        )
+    
+    def test_dataloader(self):
+        return DataLoader(
+            self.test_dataset,
+            batch_size=self.config.test_dataloader.batch_size,
+            shuffle=self.config.test_dataloader.shuffle,
+            num_workers=self.config.test_dataloader.num_workers,
+            pin_memory=self.config.test_dataloader.pin_memory,
+        )
